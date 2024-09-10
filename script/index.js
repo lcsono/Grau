477ebb59b42487d7c1d1888d7cde94378f2d9592
Matheus Alves de Oliveira <matheus.adeoliveira11@gmail.com>
2024-09-09 22:59:59 -0300

    feat: integrate register page with external API

--- script/index.js
+++ script/index.js
@@ -1,62 +1,136 @@
-const BASE_URL = 'https://go-wash-api.onrender.com/api';
-const GOWASH_SESSION = '0hGqRHf0q38ETNgEcJGce30LcPtuPKo48uKtb7Oj';
+class RegisterHandler {
+	#BASE_URL;
+	#GOWASH_SESSION;
 
-function validateData({ name, email, password, cpfCnpj, birthDate, terms }) {
-	if (!name || !email || !password || !cpfCnpj || !birthDate) {
-		alert('Preencha os dados necessários');
-
-		return false;
+	constructor() {
+		this.#BASE_URL = 'https://go-wash-api.onrender.com/api';
+		this.#GOWASH_SESSION = '0hGqRHf0q38ETNgEcJGce30LcPtuPKo48uKtb7Oj';
 	}
 
-	if (!terms) {
-		alert('É preciso preencher os termos');
+	#validateData({ name, email, password, cpf_cnpj, birthday }) {
+		if (!name || !email || !password || !cpf_cnpj || !birthday) {
+			alert('Preencha os dados necessários');
 
-		return false;
+			return false;
+		}
+
+		const parsedBirthDate = new Date(birthday);
+		parsedBirthDate.setHours(parsedBirthDate.getHours() + 3); // GMT0300
+
+		const now = new Date();
+
+		if (parsedBirthDate > now) {
+			alert('A data precisa estar no passado');
+
+			return false;
+		}
+
+		if (now.getFullYear() - parsedBirthDate.getFullYear() < 18) {
+			alert('O usuário precisa ter no mínimo 18 anos');
+
+			return false;
+		}
+
+		return true;
 	}
 
-	return true;
-}
-
-function resetValues(inputList) {
-	for (const input of inputList) {
-		input.value = '';
-		input.checked = 0;
+	#resetValues(inputList) {
+		for (const input of inputList) {
+			input.value = '';
+			input.checked = 0;
+		}
 	}
-}
 
-async function register() {
-	const nameInput = document.querySelector('#input-name');
-	const emailInput = document.querySelector('#input-email');
-	const passwordInput = document.querySelector('#input-password');
-	const cpfCnpjInput = document.querySelector('#input-cpf-cnpj');
-	const birthDateInput = document.querySelector('#input-birth-date');
-	const termsInput = document.querySelector('#input-terms');
+	#fetchFormData() {
+		const usernameInput = document.querySelector('#input-username');
+		const userDocumentInput = document.querySelector('#input-cpf-cnpj');
+		const birthDateInput = document.querySelector('#input-birth-date');
+		const emailInput = document.querySelector('#input-email');
+		const passwordInput = document.querySelector('#input-password');
+		const termsInput = document.querySelector('#input-terms');
 
-	const body = {
-		name: nameInput.value,
-		email: emailInput.value,
-		password: passwordInput.value,
-		cpfCnpj: cpfCnpjInput.value,
-		birthDate: birthDateInput.value,
-		checkedTerms: termsInput.checked,
-	};
+		const data = {
+			name: usernameInput.value,
+			email: emailInput.value,
+			password: passwordInput.value,
+			cpf_cnpj: userDocumentInput.value,
+			birthday: birthDateInput.value,
+			terms: termsInput.checked,
+			user_type_id: 1,
+		};
 
-	if (!validateData(body)) {
-		resetValues([
-			nameInput,
-			emailInput,
-			passwordInput,
-			cpfCnpjInput,
-			birthDateInput,
-			termsInput,
-		]);
+		if (!this.#validateData(data)) {
+			this.#resetValues([
+				usernameInput,
+				emailInput,
+				passwordInput,
+				userDocumentInput,
+				birthDateInput,
+				termsInput,
+			]);
+
+			return;
+		}
+
+		return data;
+	}
+
+	#getExternalAPIFetchOptions(body) {
+		return {
+			method: 'POST',
+			body: JSON.stringify(body),
+			headers: {
+				'Content-Type': 'application/json',
+				Cookie: `gowash_session=${this.#GOWASH_SESSION}`,
+			},
+		};
+	}
+
+	async execute() {
+		const termsInput = document.querySelector('#input-terms');
+
+		if (!termsInput.checked) {
+			alert('Aceite os termos para continuar');
+
+			return;
+		}
+
+		const body = this.#fetchFormData();
+
+		if (!body) {
+			return;
+		}
+
+		console.log({ body });
+
+		const fetchOptions = this.#getExternalAPIFetchOptions(body);
+
+		const response = await fetch(this.#BASE_URL + '/user', fetchOptions);
+
+		console.log(response);
 	}
 }
 
 document
-	.querySelector('input[type="button"]')
+	.querySelector('#icon-input-password')
 	.addEventListener('click', (ev) => {
 		ev.preventDefault();
 
-		register();
+		const input = document.querySelector('#input-password');
+
+		if (input.type === 'password') {
+			input.type = 'text';
+		} else {
+			input.type = 'password';
+		}
+	});
+
+document
+	.querySelector('input[type="button"]')
+	.addEventListener('click', async (ev) => {
+		ev.preventDefault();
+
+		const registerHandler = new RegisterHandler();
+
+		await registerHandler.execute();
 	});
