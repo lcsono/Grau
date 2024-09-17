703f6246955440e2f186696a2ff9c447b66768f3
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2024-09-17 13:18:29 -0300

    [ADD and UPD] Adicionando novo icone e adiconando múisica

--- script/index.js
+++ script/index.js
@@ -1,22 +1,29 @@
-let slideIndex = 0;
-showSlides(slideIndex);
+document.addEventListener('DOMContentLoaded', () => {
+  const audio = document.getElementById('backgroundMusic');
+  const logo = document.getElementById('audioControl');
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slide");
-  if (n >= slides.length) { slideIndex = 0 }
-  if (n < 0) { slideIndex = slides.length - 1 }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (!audio || !logo) {
+    console.error('Elemento de áudio ou logo não encontrado.');
+    return;
   }
 
-  slides[slideIndex].style.display = "block";
-}
+  // Verifica se a música está em reprodução
+  let isPlaying = !audio.paused; // Atualiza o estado baseado na reprodução inicial
 
-function changeSlide(n) {
-  showSlides(slideIndex += n);
-}
+  // Atualiza a imagem da logo com base no estado da música
+  logo.src = isPlaying ? './img/music.png' : './img/bigode.png';
 
-setInterval(() => {
-  changeSlide(1);
-}, 3000);
+  // Adiciona um evento de clique ao logo para controlar a reprodução da música
+  logo.addEventListener('click', () => {
+    if (isPlaying) {
+      audio.pause();
+      logo.src = './img/bigode.png'; // Imagem quando a música está pausada
+    } else {
+      audio.play();
+      logo.src = './img/music.png'; // Imagem quando a música está tocando
+    }
+
+    // Se a música estava tocando, agora estará pausada e vice-versa
+    isPlaying = !isPlaying;
+  });
+});
